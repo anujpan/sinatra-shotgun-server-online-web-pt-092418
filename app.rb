758a358198e321b7823c8a701f092387d76de76f
Anuj Panchal <anujpan1@gmail.com>
2019-01-03 18:24:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! hello hello"
+    "Welcome to your app!!!! hello hello sucka"
   end
 
 end
